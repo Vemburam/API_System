d0213e5b639cb8de4f9b77ced2ac6d488a03de78
Kaliyappan1 <yappank17@gmail.com>
2025-06-21 17:51:18 +0530

    removed elastic ip and updated auto generate security groups

--- main.tf
+++ main.tf
@@ -14,57 +14,63 @@ provider "aws" {
   region     = var.region
 }
 
-resource "aws_security_group" "splunk_sg" {
-  name        = "splunk-security-group"
-  description = "Security group for Splunk server"
-
-  ingress { 
-    from_port = 22
-    to_port = 22 
-    protocol = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    }
-
-    ingress { 
-    from_port = 8000 
-    to_port = 8000 
-    protocol = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    }
-
-    ingress { 
-    from_port = 8089 
-    to_port = 8089 
-    protocol = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    }
-
-   egress { 
-    from_port = 0 
-    to_port = 0 
-    protocol = "-1" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    }
+# Generate unique suffix for SG
+resource "random_id" "sg_suffix" {
+  byte_length = 2
 }
 
+# Security Group with unique name
+resource "aws_security_group" "splunk_sg" {
+  name        = "splunk-security-group-${random_id.sg_suffix.hex}"
+  description = "Security group for Splunk server"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8089
+    to_port     = 8089
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance
 resource "aws_instance" "splunk_server" {
-  ami                  = lookup(var.ami_map, var.region, "")
-  instance_type        = var.instance_type
-  key_name             = var.key_name
+  ami                    = lookup(var.ami_map, var.region, "")
+  instance_type          = var.instance_type
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.splunk_sg.id]
-  associate_public_ip_address = var.elastic_ip_needed
 
   root_block_device {
     volume_size = var.storage_size
   }
 
   tags = {
-    Name = var.instance_name
-    AutoStop = true
-    Owner = var.usermail
-    UserEmail = var.usermail
+    Name          = var.instance_name
+    AutoStop      = true
+    Owner         = var.usermail
+    UserEmail     = var.usermail
     RunQuotaHours = var.quotahours
-    Category = var.category
+    Category      = var.category
   }
 
   provisioner "remote-exec" {
@@ -81,21 +87,17 @@ resource "aws_instance" "splunk_server" {
   }
 }
 
-resource "aws_eip" "splunk_eip" {
-  count    = var.elastic_ip_needed ? 1 : 0
-  instance = aws_instance.splunk_server.id
-  vpc      = true
-}
-
+# Ansible inventory output
 resource "local_file" "ansible_inventory" {
   filename = "inventory.ini"
 
   content = <<EOF
 [splunk]
-${var.instance_name} ansible_host=${var.elastic_ip_needed ? aws_eip.splunk_eip[0].public_ip : aws_instance.splunk_server.public_ip} ansible_user=ec2-user
+${var.instance_name} ansible_host=${aws_instance.splunk_server.public_ip} ansible_user=ec2-user
 EOF
 }
 
+# Ansible group vars output
 resource "local_file" "ansible_group_vars" {
   filename = "group_vars/all.yml"
 
--- outputs.tf
+++ outputs.tf
@@ -15,15 +15,15 @@ output "instance_id" {
 
 output "public_or_elastic_ip" {
   description = "Elastic IP if assigned, otherwise public IP"
-  value       = var.elastic_ip_needed ? aws_eip.splunk_eip[0].public_ip : aws_instance.splunk_server.public_ip
+  value       = aws_instance.splunk_server.public_ip
 }
 
 output "splunk_ssh_string" {
   description = "SSH command to connect to the Splunk instance"
-  value       = "ssh -i ${var.key_name}.pem ec2-user@${var.elastic_ip_needed ? aws_eip.splunk_eip[0].public_ip : aws_instance.splunk_server.public_ip}"
+  value       = "ssh -i ${var.key_name}.pem ec2-user@${aws_instance.splunk_server.public_ip}"
 }
 
 output "ansible_ssh_string" {
   description = "SSH command to connect to the Splunk instance"
-  value       = "ssh ec2-user@${var.elastic_ip_needed ? aws_eip.splunk_eip[0].public_ip : aws_instance.splunk_server.public_ip}"
+  value       = "ssh ec2-user@${aws_instance.splunk_server.public_ip}"
 }
